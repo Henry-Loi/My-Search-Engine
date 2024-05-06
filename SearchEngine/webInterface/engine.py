11105a52ac419a03d6d5ba5287bb472560a2ebed
Henry-Loi <91612066+Henry-Loi@users.noreply.github.com>
2024-05-06 10:48:04 +0800

    add parent link

--- SearchEngine/webInterface/engine.py
+++ SearchEngine/webInterface/engine.py
@@ -5,6 +5,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import numpy as np
+from nltk.stem import PorterStemmer
 
 ### TODO:
 # 1. Implement stemming
@@ -14,7 +15,16 @@ class SearchEngine:
     def __init__(self, pages, indexer):
         self.pages = pages
         self.indexer = indexer
+        self.stopwords = self.load_stopwords()
         self.vectorizer = TfidfVectorizer()
+        self.stemmer = PorterStemmer()
+
+    def load_stopwords(self):
+        stopwords = set()
+        with open("stopwords.txt", "r") as file:
+            for line in file:
+                stopwords.add(line.strip())
+        return stopwords
 
     def calculate_tfidf(self):
         docs = [page.title for page in self.pages]
@@ -32,12 +42,35 @@ class SearchEngine:
         indices = np.argsort(cosine_similarities)[::-1]
         ranked_pages = [(self.pages[int(i)], cosine_similarities[i]) for i in indices]
         return ranked_pages[:50]
+    
+    def query_preprocessing(self, query):
+        # TODO: is case insensitive?
+        query = query.lower()
+        
+        # Stemming
+        query = self.stemmer.stem(query)
+        query = query.split(" ")
+        
+        # stopwords removal
+        query = [word for word in query if word not in self.stopwords]
+        
+        #convert back to string
+        query = " ".join(query)
+        
+        return query
 
     def search(self, query):
+        query = self.query_preprocessing(query)
+
         ranked_pages = self.rank_documents(query)
         outputs = []
+
         for page, score in ranked_pages:
+            # extract child links from the child_link_list
             child_link_list = page.child_link_list.split(" ") if page.child_link_list else []
+
+            # extract parent links
+            parent_link_list = self.pages.filter(child_link_list__contains=page.url)
 
             output = {
                 'page_title': page.title,
@@ -45,7 +78,7 @@ class SearchEngine:
                 'last_modification_date': page.last_modification_date,
                 'page_size': page.page_size,
                 'keywords': [(index.keyword_id.keyword, index.frequency) for index in self.indexer if index.page_id == page],
-                'parent_links': "None",
+                'parent_links': parent_link_list,
                 'child_links': child_link_list,
                 'score': score
             }
